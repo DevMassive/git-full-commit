3bbb45b3f1edbafa006b081eeda1088b06d2ab19
devmassive <massivedeveloper@gmail.com>
2025-09-22 08:56:17 +0900

    Revert "feat: remove auto add -A on launch"
    This reverts commit cf1b147a9f841a9a68fa7bb0985022b721bdb775.

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 use anyhow::{Result, bail};
 use std::path::PathBuf;
+use std::process::Command as OsCommand;
 
 pub mod app_state;
 pub mod command;
@@ -15,7 +16,22 @@ pub fn run(repo_path: PathBuf) -> Result<()> {
         bail!("fatal: not a git repository (or any of the parent directories): .git");
     }
 
+    let staged_diff_output = OsCommand::new("git")
+        .arg("diff")
+        .arg("--staged")
+        .current_dir(&repo_path)
+        .output()?;
+
+    if staged_diff_output.stdout.is_empty() {
+        git::add_all(&repo_path)?;
+    }
+
     let files = git::get_diff(repo_path.clone());
+
+    if files.is_empty() {
+        bail!("No changes found.");
+    }
+
     ui::tui_loop(repo_path.clone(), files);
 
     Ok(())
